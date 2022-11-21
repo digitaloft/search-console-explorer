9d2fc825066c39c8da4a3cb0cc7e3a6de54ef125
digitaloft <118721765+digitaloft@users.noreply.github.com>
2022-11-21 09:53:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,11 +49,6 @@ with tab1:
 
     st.write("")
 
-    # Convert secrets from the TOML file to strings
-    clientSecret = str(st.secrets["installed"]["client_secret"])
-    clientId = str(st.secrets["installed"]["client_id"])
-    redirectUri = str(st.secrets["installed"]["redirect_uris"][0])
-
     st.markdown("")
 
     if "my_token_input" not in st.session_state:
